6092d2268122add3a0b9901e37f29783fbd3b2e3
Muhammad-abdullah-official <amabdullah643@gmail.com>
2026-01-18 22:37:05 +0500

    python from basic

---
+++ pythonBasics.py
@@ -0,0 +1,32 @@
+'''
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("Sum =", x + y)
+
+
+
+num = int(input('Enter a number: '    ))
+if num % 2==0:
+    print(num, 'even number')
+
+else:
+    print(num, 'odd number')
+    
+  
+
+num = int(input('Enter a number: '))
+for i in range(1, num+1):
+    if i % 2 == 0:
+        print(i, " is even,")
+    else:
+        print(i, " is odd,")
+        
+i = 2
+while i <= 6:
+    print('num is', i)
+    i +=1
+    
+'''
+
