b28890a2a877e2388c6275d7b90904e2141f87ca
Muhammad-abdullah-official <amabdullah643@gmail.com>
2026-04-24 11:42:49 +0500

    day3

---
+++ Day3/app.js
@@ -0,0 +1,50 @@
+// question 1
+console.log('-------------Q NO 1-----------')
+for (let i = 0; i < 5; i++) {
+    let line = ''
+    for (let j = 0; j <= i; j++) {
+        line += '*'
+    }
+console.log(line)
+}
+
+console.log('-------------Q NO 2-----------')
+
+for (let i = 5; i >= 0; i--) {
+    let line = ''
+    for (let j = 0; j <= i; j++) {
+        line += '*'
+    }
+console.log(line)
+}
+
+
+console.log('-------------Q NO 3-----------')
+for (let i = 0; i < 5; i++) {
+    let line = ''
+    let space = ''
+    for (let j = 4; j > i; j--) {
+        space += ' '
+    }
+    for (let k = 0; k < 2 * i +1; k++) {
+        line += '*'
+    }
+    console.log(space + line) // concate
+    
+}
+
+console.log('-------------Q NO 4-----------')
+for (let i = 5; i > 0; i--) {
+    let line = ''
+    let space = ''
+    
+    for (let j = 0; j < 5-i; j++) {
+        space += ' '
+    }
+    for (let k = 0; k < 2*i -1; k++) {
+        line += '*'
+    }
+    
+    console.log(space + line)
+    
+}
