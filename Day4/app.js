4c67a7cce21f3fcd21a5b27b0d5e0933313c042c
Muhammad-abdullah-official <amabdullah643@gmail.com>
2026-05-04 16:09:14 +0500

    day4

---
+++ Day4/app.js
@@ -0,0 +1,16 @@
+console.log('-------------Q NO 01-----------')
+
+let n1 = 12345
+let n1arr = []
+
+while (n1 > 0) {
+        n1arr.push(n1%10)
+        n1 = Math.floor(n1/10)
+}
+console.log(n1arr.reverse())
+
+console.log('-------------Q NO 02-----------')
+let n2 = 12.34
+console.log(n2*100)
+
+console.log('-------------Q NO 03-----------')
